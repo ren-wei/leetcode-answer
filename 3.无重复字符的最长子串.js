b7895adc9f04c420fd07c3c70cbdc51dc78fac15
ren-wei <2665176834@qq.com>
2019-09-28 19:40:28 +0800

    runtime beats 98.97 % of javascript submissions

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -9,17 +9,14 @@
  */
 var lengthOfLongestSubstring = function(s) {
   var len = s.length
-  var set = new Set()
+  var map = new Map()
   var result=0, i=0, j=0
   while (i < len && j < len) {
-    if (!set.has(s[j])) {
-      set.add(s[j])
-      j++
-      result = Math.max(result, j - i)
-    } else {
-      set.delete(s[i])
-      i++
+    if (map.has(s[j])) {
+      i = Math.max(map.get(s[j]), i)
     }
+    result = Math.max(result, j+1-i)
+    map.set(s[j], ++j)
   }
   return result
 };
