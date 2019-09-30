aee22b5497584589c24d84f8b1e07d978a552e8d
ren-wei <2665176834@qq.com>
2019-10-01 00:53:48 +0800

    runtime beats 98.84 % of javascript submissions

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -10,42 +10,42 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-  var len = s.length
-  if (!len) {
-    return s
-  }
-  var result = s[0]
-  for (var i=0;i<len;i++) {
-    // 以当前字符为中心
-    var left = i
-    var right = i
-    while (left - 1 >= 0 && right + 1 < len) {
-      if (s[left-1] === s[right+1]) {
-        left--
-        right++
-      } else {
-        break
-      }
-    }
-    if (right + 1 - left > result.length) {
-      result = s.substring(left, right+1)
-    }
-    // 以当前字符与下一字符之间为中心
-    left = i + 1
-    right = i
-    while (left - 1 >= 0 && right + 1 < len) {
-      if (s[left-1] === s[right+1]) {
-        left--
-        right++
-      } else {
-        break
-      }
-    }
-    if (right + 1 - left > result.length) {
-      result = s.substring(left, right+1)
-    }
-  }
+  // Manacher算法
 
-  return result
+  // 使用`#`填充使其长度为奇数
+  var slist = s.split('')
+  var ns = '#' + slist.join('#') + '#'
+
+  // 定义p数组
+  var p = new Array(ns.length).fill(0)
+  var id = 0       // 用于对称性的子串中心的下标
+  var right = 0    // id子串的最右边界下标
+  var reCenter = 0 // 最大回文子串中心下标
+  var reLen = 0    // 最大回文子串长度
+
+  // 开始遍历
+  for (var i=0;i<ns.length;i++) {
+    // 在以id为中心的回文内,当前位置和关于id对称的位置相同
+    if (right > i) {
+      p[i] = Math.min(right - i, p[2*id - i])
+    } else {
+      p[i] = 1
+    }
+    // 扩展回文子串
+    while (i-p[i] >= 0 && ns[i+p[i]] === ns[i-p[i]]) {
+      p[i]++
+    }
+    // 当i在id子串右边界之外时更新id子串
+    if (i + p[i] > right) {
+      id = i
+      right = i + p[i]
+    }
+    // 更新结果
+    if (p[i] > reLen) {
+      reCenter = i
+      reLen = p[i]
+    }
+  }
+  return s.substr(Math.ceil((reCenter-reLen)/2), reLen - 1)
 };
 // @lc code=end
