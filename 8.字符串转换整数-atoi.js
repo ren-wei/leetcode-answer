060cfa4701fe114373fcc4ee8e0bc47ab71f131b
ren-wei <demonre@foxmail.com>
2019-10-03 21:20:53 +0800

    runtime beats 99.46 % of javascript submissions

--- 8.字符串转换整数-atoi.js
+++ 8.字符串转换整数-atoi.js
@@ -11,66 +11,7 @@
  */
 var myAtoi = function(str) {
   str = str.replace(/^\s+/,'')
-  var answer = 0
-  var len = 0
-  var plus = true
-  if (str[0] === '-') {
-    plus = false
-    str = str.substring(1)
-  } else if (str[0] === '+') {
-    str = str.substring(1)
-  }
-  var nlist = str.split('')
-
-  var maxi = Math.min(9, nlist.length)
-
-  // 对前9位赋值
-  for (var i=0;i<maxi;i++) {
-    var value = Number(nlist[i])
-    if (isNaN(value) || nlist[i] === ' ') {
-      if (plus) {
-        return answer
-      } else {
-        return -answer
-      }
-    } else {
-      answer = answer * 10 + value
-      if (answer === 0) {
-        maxi++
-      }
-    }
-  }
-
-  // 对第10位赋值
-  var value = Number(nlist[maxi])
-  if (isNaN(value) || nlist[maxi] === ' ') {
-    if (plus) {
-      return answer
-    } else {
-      return -answer
-    }
-  }
-  if (plus) {
-    if (2147483647 - value < answer * 10) {
-      return 2147483647
-    }
-    answer = answer * 10 + value
-    // 如果有第11位返回最大值
-    if (nlist.length > maxi+1 && (!isNaN(Number(nlist[maxi+1]))) && nlist[maxi+1] !== ' ') {
-      return 2147483647
-    } else {
-      return answer
-    }
-  } else {
-    if (2147483648 - value < answer * 10) {
-      return -2147483648
-    }
-    answer = answer * 10 + value
-    if (nlist.length > maxi+1 && (!isNaN(Number(nlist[maxi+1]))) && nlist[maxi+1] !== ' ') {
-      return -2147483648
-    } else {
-      return -answer
-    }
-  }
+  var answer = str.match(/^[\+\-]?\d+/)
+  return Math.max(Math.min(answer ? Number(answer[0]) : 0, 2147483647) ,-2147483648)
 };
 // @lc code=end
