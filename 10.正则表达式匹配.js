6b75281a847b01f8dfd705e7528fa6c1578ae6d5
ren-wei <demonre@foxmail.com>
2019-10-05 22:35:18 +0800

    runtime beats 99.85 % of javascript submissions

--- 10.正则表达式匹配.js
+++ 10.正则表达式匹配.js
@@ -11,28 +11,22 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-  var memo = Array.from({length: s.length+1}, function() {
-    return new Array(p.length+1)
+  var dp = Array.from({length: s.length+1}, function() {
+    return new Array(p.length+1).fill(false)
   })
-
-  function dp(i, j) {
-    if (memo[i][j] === undefined) {
-      var ans
-      if (j === p.length) {
-        ans = i === s.length
+  
+  dp[s.length][p.length] = true
+  for (var i=s.length;i>=0;i--) {
+    for (var j=p.length-1;j>=0;j--) {
+      var first = i < s.length && (p[j] === s[i] || p[j] === '.')
+      if (j+1 < p.length && p[j+1] === '*') {
+        dp[i][j] = dp[i][j+2] || first && dp[i+1][j]
       } else {
-        var first = i < s.length && (p[j] === s[i] || p[j] === '.')
-        if (j+1 < p.length && p[j+1] === '*') {
-          ans = dp(i, j+2) || first && dp(i+1, j)
-        } else {
-          ans = first && dp(i+1, j+1)
-        }
+        dp[i][j] = first && dp[i+1][j+1]
       }
-      memo[i][j] = ans
     }
-    return memo[i][j]
   }
 
-  return dp(0, 0)
+  return dp[0][0]
 };
 // @lc code=end
