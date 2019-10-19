dd67522d784c56e6c80f62da423ba2bd14c971b5
ren-wei <demonre@foxmail.com>
2019-10-19 10:25:47 +0800

    runtime beats 95.88 % of javascript submissions

---
+++ 24.两两交换链表中的节点.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=javascript
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+  if (!head) {
+    return head
+  }
+  var prev = head
+  var cur = prev.next
+  if (!cur) {
+    return head
+  }
+  var next = cur.next
+  head = cur
+  cur.next = prev
+  prev.next = next
+
+  while (next) {
+    cur = next.next
+    if (!cur) {
+      break
+    }
+    prev.next = cur
+    prev = next
+    next = cur.next
+    cur.next = prev
+  }
+  prev.next = next
+
+  return head
+}
+// @lc code=end
+
