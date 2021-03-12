edbe30e6edfa5905b051d6e9593ecb689ee43e77
ren-wei <demonre@foxmail.com>
2021-03-12 18:11:00 +0800

    test: 增加34题测试

---
+++ test/34.在排序数组中查找元素的第一个和最后一个位置.test.js
@@ -0,0 +1,7 @@
+const searchRange = require('../34.在排序数组中查找元素的第一个和最后一个位置')
+
+test("34.在排序数组中查找元素的第一个和最后一个位置", () => {
+    expect(searchRange([5,7,7,8,8,10], 8)).toEqual([3, 4])
+    expect(searchRange([5,7,7,8,8,10], 6)).toEqual([-1, -1])
+    expect(searchRange([], 0)).toEqual([-1, -1])
+})
